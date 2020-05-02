b8b48d58168eb05c48557c775c5b63a40b51792b
ckmonish2000 <ckmonish2000@gmail.com>
2020-05-03 01:31:50 +0530

    second commit

--- car.py
+++ car.py
@@ -1,6 +1,13 @@
 import gym
+import matplotlib
 env=gym.make('MountainCar-v0')
 env.reset()
+
+print(env.observation_space.high)
+print(env.observation_space.low)
+print(env.action_space.n)
+
+DISCRETE_OS_SIZE=[20] * len(env.observation_space.high)
 
 done= False
 
@@ -8,5 +15,5 @@ while not done:
     action=2
     new_state,reward,done,_=env.step(action)
     env.render()
-
+    print(new_state)
 env.close()
